6b7695fe10018654bbf2340b268c6973f6bea378
David Liew <davidliew1999@gmail.com>
2021-10-04 20:29:14 +0800

    Finish Metalplex navBar CSS

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,8 +5,12 @@ background: Transparent;
 border: none;
 color: ${({ theme }) => theme.text};
 cursor: pointer;
-font-size:0.8rem;
 padding: 0.6rem;
+font-size: 16px;
+line-height: 19px;
+:hover {
+  color: red;
+}
 }
 `;
 
@@ -15,6 +19,12 @@ const NavBarWrapper = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
+  background: ${({ theme }) => theme.background};
+  border-radius: 8px;
+  display: flex;
+  height: 56px;
 `;
 
 export const NavBar = ({ headers }: { headers: string[] }) => {
